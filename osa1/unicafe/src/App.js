f60187adf7bde463cfa3881211bdae4a7d6d5741
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-11 15:31:04 +0200

    uncafe 1.9 show statistics only if feedback given

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -3,17 +3,27 @@ import React, { useState } from 'react'
 const Statistics = (props) => {
   const {good, neutral, bad, all, average, positive} = props
 
-  return (
-    <div>
-        <h1>statstics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+  if( all > 0) {
+    return (
+      <div>
+          <h1>statistics</h1>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive} %</p>
+        </div>
+    )  
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
       </div>
-  )
+    )
+  }
+  
 }
 
 const App = () => {
