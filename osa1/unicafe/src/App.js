38cb5670194dad251d67b3672fee3d8e38174880
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-11 15:58:37 +0200

    uncafe 1.11 changed statistics to be in table format

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -13,7 +13,10 @@ const StatisticsLine = (props) => {
         
         return (
           <>
-            <p>{text} {value}</p>
+            <tr>
+              <td>{text}</td>
+              <td>{value}</td>
+            </tr>
           </>
         )
 }
@@ -25,12 +28,16 @@ const Statistics = (props) => {
     return (
       <div>
           <h1>statistics</h1>
-          <StatisticsLine text="good" value={good} />
-          <StatisticsLine text="neutral" value={neutral} />
-          <StatisticsLine text="bad" value={bad} />
-          <StatisticsLine text="all" value={all} />
-          <StatisticsLine text="average" value={average} />
-          <StatisticsLine text="positive" value={positive + "%"} />
+          <table>
+            <tbody>
+              <StatisticsLine text="good" value={good} />
+              <StatisticsLine text="neutral" value={neutral} />
+              <StatisticsLine text="bad" value={bad} />
+              <StatisticsLine text="all" value={all} />
+              <StatisticsLine text="average" value={average} />
+              <StatisticsLine text="positive" value={positive + "%"} />
+            </tbody>
+          </table>
         </div>
     )  
   } else {
