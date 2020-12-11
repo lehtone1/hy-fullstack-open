2b52eca16e1f347cfcc8d0cc1d18f03983015ad4
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-11 17:46:45 +0200

    anekdootit 1.13 added anecdote voting

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 const App = (props) => {
   const { anecdotes } = props;
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(6));
+  const randomNumber = Math.floor(Math.random() * 6);
 
-  const randomSelect = () => {
-    const randomNumber = Math.floor(Math.random() * 6);
-
-    return (
-      setSelected(randomNumber)
-    );
-  };
+  const setToSelected = () => setSelected(randomNumber);
+  const setToPoints = () => {
+    const copyPoints = [...points];
+    copyPoints[selected] += 1;
+    setPoints(copyPoints);
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <button type="button" onClick={() => randomSelect()}>next anecdote</button>
+      <p>has {points[selected]} votes</p>
+      <button type="button" onClick={() => setToPoints()}>vote</button>
+      <button type="button" onClick={() => setToSelected()}>next anecdote</button>
     </div>
   );
 };
