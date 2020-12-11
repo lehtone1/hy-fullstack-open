fb16a38869799beaa679ed286e6b4d200214e589
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-11 18:06:37 +0200

    anekdootit 1.14 show anecdote with most votes

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -5,9 +5,12 @@ const App = (props) => {
   const { anecdotes } = props;
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(new Uint8Array(6));
-  const randomNumber = Math.floor(Math.random() * 6);
+  const indexOfMostPonts = points.reduce((iMax, x, i, arr) => x>arr[iMax] ? i:iMax, 0)
 
-  const setToSelected = () => setSelected(randomNumber);
+  const setToSelected = () => {
+    const randomNumber = Math.floor(Math.random() * 6);
+    setSelected(randomNumber);
+  }
   const setToPoints = () => {
     const copyPoints = [...points];
     copyPoints[selected] += 1;
@@ -16,10 +19,14 @@ const App = (props) => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <button type="button" onClick={() => setToPoints()}>vote</button>
       <button type="button" onClick={() => setToSelected()}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[indexOfMostPonts]}</p>
+      <p>has {points[indexOfMostPonts]} votes</p>
     </div>
   );
 };
