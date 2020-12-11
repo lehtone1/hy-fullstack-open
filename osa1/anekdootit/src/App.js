6027f6c318d3c417a0118f33ce64581ca83d008c
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-11 18:56:06 +0200

    anekdootit - made style changes according to eslint

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -5,28 +5,40 @@ const App = (props) => {
   const { anecdotes } = props;
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(new Uint8Array(6));
-  const indexOfMostPonts = points.reduce((iMax, x, i, arr) => x>arr[iMax] ? i:iMax, 0)
+  const indexOfMostPonts = points.reduce((iMax, x, i, arr) => (x > arr[iMax] ? i : iMax), 0);
 
   const setToSelected = () => {
     const randomNumber = Math.floor(Math.random() * 6);
     setSelected(randomNumber);
-  }
+  };
   const setToPoints = () => {
     const copyPoints = [...points];
     copyPoints[selected] += 1;
     setPoints(copyPoints);
-  }
+  };
 
   return (
     <div>
       <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
+      <p>
+        has
+        {' '}
+        {points[selected]}
+        {' '}
+        votes
+      </p>
       <button type="button" onClick={() => setToPoints()}>vote</button>
       <button type="button" onClick={() => setToSelected()}>next anecdote</button>
       <h1>Anecdote with most votes</h1>
       <p>{anecdotes[indexOfMostPonts]}</p>
-      <p>has {points[indexOfMostPonts]} votes</p>
+      <p>
+        has
+        {' '}
+        {points[indexOfMostPonts]}
+        {' '}
+        votes
+      </p>
     </div>
   );
 };
