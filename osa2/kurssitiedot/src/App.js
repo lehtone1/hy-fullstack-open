4ad24ad4f9768edac41eff3f0b6c9db2f0149ddd
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-15 18:40:58 +0200

    2.3*: kurssitiedot step8 - calculatng total courses with reduce

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -27,10 +27,11 @@ const Part = ({part}) => {
 };
 
 const Total = ({parts}) => {
+  const total = parts.reduce(((accumulator, part) => accumulator + part.exercises), 0)
   return (
-    <>
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    </>
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
   )
 };
 
