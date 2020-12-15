d83d5e16d6dda53df42a3986d0093074fff113fd
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-15 18:51:49 +0200

    2.5: erillinen moduuli - separating components to different modules from App.js

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,49 +1,5 @@
 import React from 'react';
-
-const Header = ({name}) => {
-  return (
-    <> 
-      <h1>{name}</h1>
-    </>
-  )
-};
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map((part) =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-};
-
-const Part = ({part}) => {
-  return (
-    <>
-      <p>{part.name} {part.exercises}</p>
-    </>
-  )
-};
-
-const Total = ({parts}) => {
-  const total = parts.reduce(((accumulator, part) => accumulator + part.exercises), 0)
-  return (
-    <p>
-      <b>total of {total} exercises</b>
-    </p>
-  )
-};
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
