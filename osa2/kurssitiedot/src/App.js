88c477d4f08f52f798b21fd88d1f55f89d99ee7c
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-15 18:27:40 +0200

    2.2: kurssitiedot step7 - refactored some props and added Course-component

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+const Header = ({name}) => {
+  return (
+    <> 
+      <h1>{name}</h1>
+    </>
+  )
+};
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+};
+
+const Part = ({part}) => {
+  return (
+    <>
+      <p>{part.name} {part.exercises}</p>
+    </>
+  )
+};
+
+const Total = ({parts}) => {
+  return (
+    <>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </>
+  )
+};
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course key={course.id} course={course} />
+    </div>
+  )
+}
+
+export default App;
