afbee96f78322bd6041e2b764281474bb5934411
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-16 15:14:08 +0200

    2.8: puhelinluettelo step3 - possibility to add a phone number

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1231244',
+    }
   ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
   const names = persons.map((person) => person.name)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   const addName = (event) => {
@@ -17,10 +25,12 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       };
       setPersons(persons.concat(newPerson));
-      setNewName(''); 
+      setNewName('');
+      setNewNumber(''); 
     }
   }
 
@@ -33,12 +43,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => 
-        <p>{person.name}</p>
+        <p>{person.name} {person.number}</p>
       )}
     </div>
   )
