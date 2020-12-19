735578958ce86d5fbc5deb87aeb0f2830afc3bff
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-19 10:30:47 +0200

    3.3: puhelinluettelon backend step3 - showing individual person

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id )
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/info', (req, res) => {
   const now = new Date(Date.now())
   res.send(
