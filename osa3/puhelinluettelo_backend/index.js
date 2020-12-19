05381d93c8a2265dc43f9794b78aa5c8f8231c13
Eero Lehtonen <eero.lehtonen@aalto.fi>
2020-12-19 10:56:44 +0200

    3.4: puhelinluettelon backend step4 - implemented person deletion

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -48,6 +49,13 @@ app.get('/info', (req, res) => {
     `<p>Phonebook has info for ${persons.length} people</p>
     <p>${now}</p>`
   )
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter((person) => person.id !== id)
+
+  res.status(204).end()
 })
 
 const PORT = 3001;
