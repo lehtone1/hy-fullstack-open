fd7826076a248dda5e6907a9a714caa1a765b383
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-01-07 18:27:03 +0200

    4.2 blogilista, step2 - reformating application structure

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const config = require('./util/config')
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URL
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
