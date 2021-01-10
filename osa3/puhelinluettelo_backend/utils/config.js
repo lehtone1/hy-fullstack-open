8dda1dcffd5b607e461b5020d904b273fbfea5bd
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-01-10 21:54:43 +0200

    puhelinluettelo_backend - configured testing and created an initial supertest

--- osa3/puhelinluettelo_backend/utils/config.js
+++ osa3/puhelinluettelo_backend/utils/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URL = process.env.MONGODB_URL
+let ENV = process.env.NODE_ENV
+let MONGODB_URL = ENV === 'test'? process.env.TEST_MONGODB_URL: process.env.MONGODB_URL
 
 module.exports = {
   PORT, MONGODB_URL
