2a06c93e02dc5acc359d610e147c03b70857d687
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-01-11 17:09:27 +0200

    4.10: blogilistan testit, step3 - test for HTTP post correct type object added and blogs count increased by 1

--- osa4/blogilista/tests/blogilista_api.test.js
+++ osa4/blogilista/tests/blogilista_api.test.js
@@ -18,6 +18,23 @@ test('the field identifying a blog should be named id', async () => {
   });
 })
 
+test('adding a blog increases the number of blogs by 1', async () => {
+  const initialBlogs = await api.get('/api/blogs')
+  const newBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  const currentBlogs = await api.get('/api/blogs')
+  expect(currentBlogs.body.length - initialBlogs.body.length).toBe(1)
+})
+
 afterAll(() => {
   mognoose.connection.close()
 })
--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
-const MONGODB_URL = process.env.MONGODB_URL
+const NODE_ENV = process.env.NODE_ENV
+const MONGODB_URL = NODE_ENV === 'test'? process.env.TEST_MONGODB_URL:process.env.MONGODB_URL
 const PORT = process.env.PORT
 
 module.exports = {
