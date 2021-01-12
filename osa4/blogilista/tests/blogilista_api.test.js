de4cd90b30d1e2a301c1b0ae20e6e8af11903a92
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-01-12 17:47:45 +0200

    Blogilista - reformatting tests

--- osa4/blogilista/tests/blogilista_api.test.js
+++ osa4/blogilista/tests/blogilista_api.test.js
@@ -5,94 +5,111 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const testingMaterial = require('./testing_material')
 
-beforeEach(async () => {
-  await Blog.deleteMany()
-  let blog = new Blog(testingMaterial.initialBlogs[0])
-  await blog.save()
-  blog = new Blog(testingMaterial.initialBlogs[1])
-  await blog.save()
-
-})
-
-test('testing database has the correct items', async () => {
-  const result = await api.get('/api/blogs')
+describe('Initial database', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany()
+    const blogs = testingMaterial.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogs.map(blog => blog.save())
+    await Promise.all(promiseArray)
   
-  const titlesGet = result.body.map(blog => blog.title)
-  const titlesInitial = [testingMaterial.initialBlogs[0].title, testingMaterial.initialBlogs[1].title]
+  })
 
-  expect(titlesGet).toEqual(titlesInitial)
+  test('has the correct items', async () => {
+    const result = await api.get('/api/blogs')
+    const resultTitles = result.body.map(blog => blog.title)
+    const initialTitles = testingMaterial.initialBlogs.map(blog => blog.title)
+    expect(resultTitles).toEqual(initialTitles)
+  })
+
+  test('returns blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blogs where the identifying field is id', async () => {
+    const result = await api.get('/api/blogs')
+    result.body.forEach(blog => {
+      //console.log(blog)
+      expect(blog.id).toBeDefined()
+    });
+  })
+
+  describe("Adding a new blog", () => {
+    test('increases the number of blogs by 1', async () => {
+      const initialBlogs = await api.get('/api/blogs')
+      const newBlog = {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      const currentBlogs = await api.get('/api/blogs')
+      expect(currentBlogs.body.length - initialBlogs.body.length).toBe(1)
+    })
+
+    test('where like field is missing adds it with value 0', async () => {
+      const newBlog = {
+        title: "React tactics",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/"
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+    
+      const blogs = await api.get('/api/blogs')
+      const addedBlog = blogs.body.find(blog => blog.title === "React tactics")
+      expect(addedBlog.likes).toBe(0)
+    })
+
+    test('without title returns status code 400', async () => {
+      const newBlog = {
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/"
+      }
+    
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+
+    test('without author returns status code 400', async () => {
+      const newBlog = {
+        title: "React tactics",
+        url: "https://reactpatterns.com/"
+      }
+    
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+  })
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
 
-test('the field identifying a blog should be named id', async () => {
-  const result = await api.get('/api/blogs')
-  result.body.forEach(blog => {
-    //console.log(blog)
-    expect(blog.id).toBeDefined()
-  });
-})
 
-test('adding a blog increases the number of blogs by 1', async () => {
-  const initialBlogs = await api.get('/api/blogs')
-  const newBlog = {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  const currentBlogs = await api.get('/api/blogs')
-  expect(currentBlogs.body.length - initialBlogs.body.length).toBe(1)
-})
 
-test('the system atomatically adds 0 for the value of likes field if the field is missing', async () => {
-  const newBlog = {
-    title: "React tactics",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/"
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
 
-  const blogs = await api.get('/api/blogs')
-  const addedBlog = blogs.body.find(blog => blog.title === "React tactics")
-  expect(addedBlog.likes).toBe(0)
-})
 
-test('if added blog has title field missing returns status code 400', async () => {
-  const newBlog = {
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/"
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
 
-test('if added blog has author field missing returns status code 400', async () => {
-  const newBlog = {
-    title: "React tactics",
-    url: "https://reactpatterns.com/"
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
+
+
+
+
+
+
+
 
 afterAll(() => {
   mognoose.connection.close()
