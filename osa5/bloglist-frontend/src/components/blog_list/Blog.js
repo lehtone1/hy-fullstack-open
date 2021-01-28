5a4b265b1e2e1a4336cbf2c9c0f53e80ea4b2005
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-01-28 09:51:23 +0200

    Blog details preview

--- osa5/bloglist-frontend/src/components/blog_list/Blog.js
+++ osa5/bloglist-frontend/src/components/blog_list/Blog.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+
+const Blog = ({ blog }) => {
+  const [visibility, setVisibility] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
+
+  const showWhenVisible = {display: visibility? "": "none"}
+  const hideWhenVisible = {display: visibility? "none": ""}
+
+  return (
+    <div style = {blogStyle}>
+      <div>
+        {blog.title}  {blog.author}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>View</button>
+        <button style={showWhenVisible} onClick={toggleVisibility}>Hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}
+      </div>
+      <div style={showWhenVisible}>
+        {blog.likes}
+      </div>
+      <div style={showWhenVisible}>
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
