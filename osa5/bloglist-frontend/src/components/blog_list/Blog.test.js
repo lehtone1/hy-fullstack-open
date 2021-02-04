07202baad1f52bfdd66dadd9f28624cfdf07aeec
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-02-04 15:22:41 +0200

    5.14: blogilistan testit, step2 - blogilista_frontend, test for blogi button click

--- osa5/bloglist-frontend/src/components/blog_list/Blog.test.js
+++ osa5/bloglist-frontend/src/components/blog_list/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Blog from './Blog'
 
 test('render correct values', () => {
@@ -24,4 +24,30 @@ test('render correct values', () => {
   expect(component.container).toHaveTextContent('Margareth Lovelylaa')
   expect(component.container).not.toHaveTextContent('www.lovewhatyoudo.com')
   expect(component.container).not.toHaveTextContent('0')
+})
+
+test('render correct values after button click', () => {
+  const data = {
+    'title': 'Doing the things I love',
+    'author': 'Margareth Lovelylaa',
+    'url': 'www.lovewhatyoudo.com',
+    'likes': 0,
+    'user': {
+      'username': 'naziboy95',
+      'name': 'Frederik Whiteman',
+      'password': 'hitler'
+    }
+  }
+
+  const component = render(
+    <Blog blog = {data}/>
+  )
+
+  const button = component.getByText('View')
+
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('www.lovewhatyoudo.com')
+  expect(component.container).toHaveTextContent('0')
+
 })
